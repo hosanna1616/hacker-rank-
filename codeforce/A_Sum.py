9feeeb341043c09f75a5845aae942474a1047a44
hosanna1616 <hosannawalle@gmail.com>
2026-02-07 23:02:44 +0300

    sum

---
+++ codeforce/A_Sum.py
@@ -0,0 +1,10 @@
+t = int(input())
+
+for _ in range(t):
+    a, b, c = map(int, input().split())
+    
+   
+    if a == b + c or b == a + c or c == a + b:
+        print("YES")
+    else:
+        print("NO")
